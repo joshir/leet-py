060e42949eb8c4bd9cb8bb8309924ad5db3944e7
joshir <hareepjoshi@gmail.com>
2023-09-15 11:37:56 -0500

    fix

--- backtracking/PermII.py
+++ backtracking/PermII.py
@@ -1,26 +1,30 @@
 from typing import List
 
 
-def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+def permuteUnique(nums: List[int]) -> List[List[int]]:
     res = []
-    used = [False] * len(nums)
+    visited = [False] * len(nums)
 
-    def dfs(path: List[int]) -> None:
-        if len(path) == len(nums):
-            res.append(path.copy())
+    def dfs(perm: List[int]) -> None:
+        if len(perm) == len(nums):
+            res.append(perm.copy())
             return
 
         for i, num in enumerate(nums):
-            if used[i]:
+            if visited[i]:
                 continue
-            if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+            if i > 0 and nums[i] == nums[i - 1] and visited[i - 1] == False:
                 continue
-            used[i] = True
-            path.append(num)
-            dfs(path)
-            path.pop()
-            used[i] = False
+            visited[i] = True
+            perm.append(num)
+            dfs(perm)
+            perm.pop()
+            visited[i] = False
 
     nums.sort()
     dfs([])
     return res
+
+
+if __name__ == '__main__':
+    print(permuteUnique([1, 2, 2]))
