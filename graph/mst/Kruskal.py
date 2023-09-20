cf5bab1dfb568d476d5d322bb441cdcbfd8bc876
joshir <hareepjoshi@gmail.com>
2023-09-20 17:26:49 -0500

    fixes and kruskal

---
+++ graph/mst/Kruskal.py
@@ -0,0 +1,19 @@
+import heapq
+
+from graph.mst.DisjointSet import DisjointSet
+
+
+def minimumSpanningTree(edges, n):
+
+    min_heap = []
+    for s, d, w in edges:
+        heapq.heappush(min_heap, [w, s, d])
+
+    dset = DisjointSet(n)
+    mst = []
+    while len(mst) < n - 1:
+        w, s, d = heapq.heappop(min_heap)
+        if not dset.union(s, d):
+            continue
+        mst.append([s, d])
+    return mst
