3cd4ca3109e346978ddbe81acb6de3b00424e448
joshir <hareepjoshi@gmail.com>
2023-09-20 17:14:53 -0500

    uf for connected components/kruskal

---
+++ graph/mst/DisjointSet.py
@@ -0,0 +1,23 @@
+class DisjointSet:
+
+    def __int__(self, size: int):
+        self.size, self.rank, self.parent = size, [], []
+        for i in range(size+1):
+            self.rank[i] = 0
+            self.parent[i] = i
+
+    def find(self, node: int):
+        if node == self.parent[node]:
+            return node
+        else :
+            self.parent[node] = self.find(self.parent[node])
+            return self.parent[node]
+
+    def union(self, u: int, v: int):
+        u, v = self.find(u), self.find(v)
+
+        if self.rank[u] == self.rank[v]:
+            self.parent[u] = v
+            self.rank[v] += 1
+        else:
+            self.parent[u if self.rank[u] < self.frank[v] else v] = self.parent[v if self.rank[u] < self.frank[v] else u]
